31a0bc81f9b9fa081e06a44d5e9bb5be745af21b
kitawanv <korney.novikov7.kn@gmail.com>
2022-06-27 19:58:08 +0300

    navigation panel

--- study/urls.py
+++ study/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from posts import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
-]
+    path('', include('posts.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
